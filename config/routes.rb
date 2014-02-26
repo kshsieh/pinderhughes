89c1e5028add4a92e005657d8fd511928bbead5d
thwang <thetimothy@gmail.com>
2014-02-26 00:04:41 -0500

    install Foundation 5, build basic pages, implement social media icon links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Pinderhughes::Application.routes.draw do
 
+  root to: 'pages#home'
+  
   namespace :admin do
     resources :biographies, only: [:edit, :update]
   end
+
+  match '/about',    to: 'pages#about',    via: 'get'
+  match '/calendar', to: 'pages#calendar', via: 'get'
+  match '/listen',   to: 'pages#listen',   via: 'get'
+  match '/watch',    to: 'pages#watch',    via: 'get'
+  match '/contact',  to: 'pages#contact',  via: 'get'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
