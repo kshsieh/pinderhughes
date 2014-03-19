3a8584c6eb31a5b6953e19056127b4b78ef45861
thwang <thetimothy@gmail.com>
2014-03-19 00:44:43 -0400

    add forms for creating blogs, tracks, videos, social media sites, events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,8 +23,7 @@ class UsersController < ApplicationController
 
   private 
     def user_params
-      params.require(:user).permit(
-        :first_name, :last_name, :biography, :email, :password,
+      params.require(:user).permit(:biography, :email, :password,
         social_media_sites: [:site_name, :link, :id, :user_id, :_destroy])
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true 
 #TODO investigate why email and pw tests are not failing when
 #validation is commented out. most likely Devise is handling it      
   # validates :email, presence: true
