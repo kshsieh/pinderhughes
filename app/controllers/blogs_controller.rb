01987e2acbc726424321fdcc3844b79dbd36ae14
Kevin Hsieh <kshsieh@wework.com>
2014-03-22 14:52:00 -0400

    before filter

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,16 @@
 class BlogsController < ApplicationController
-  load_and_authorize_resource  
-
+  before_filter :load_blog, only: :new
+  
   def index
+    @blogs = Blog.all
   end
 
   def new
+  end
+
+  private
+
+  def load_blog
+    @blog = Blog.find params[:id]
   end
 end
