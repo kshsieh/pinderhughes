cfe4be8ef73fc282374c0bda8a2f34bba7859fe6
thwang <thetimothy@gmail.com>
2014-04-05 19:24:29 -0400

    move primary_user_email method from PagesController to ApplicationController so its avavailable at users/sign_in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,12 @@
 class PagesController < ApplicationController
   
-  helper_method :primary_user_email
+  # helper_method :primary_user_email
 
   def home
   end
 
-  private
-    def primary_user_email
-      User.where(primary: true).first.email
-    end
+  # private
+  #   def primary_user_email
+  #     User.where(primary: true).first.email
+  #   end
 end
