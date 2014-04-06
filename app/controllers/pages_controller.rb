150eb172cf617bfdf3a222d41384b2a6addf3465
thwang <thetimothy@gmail.com>
2014-04-05 20:58:27 -0400

    configure mailer for dev and production environments
    change production mailer domain env variable
    
    repair action mailer for production

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,4 @@
 class PagesController < ApplicationController
-  
-  # helper_method :primary_user_email
-
   def home
   end
-
-  # private
-  #   def primary_user_email
-  #     User.where(primary: true).first.email
-  #   end
 end
