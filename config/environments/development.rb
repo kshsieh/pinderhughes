1858a1b7628210fb7eec09dbbf8a2570fdf6af14
thwang <thetimothy@gmail.com>
2014-04-05 20:58:27 -0400

    configure mailer for devise password recovery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,21 @@ Pinderhughes::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # set to false if don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["DEV_EMAIL_DOMAIN"],
+    authentication: "plain",
+    user_name: ENV["GMAIL_USERNAME"], 
+    password: ENV["GMAIL_LOGIN"],
+    enablestarttls_auto: true
+  }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
