b79a4ed4a6366a9a864ae58565925c314c111cf5
thwang <thetimothy@gmail.com>
2014-04-16 01:17:46 -0400

    add edit and remove icons to admin views, set blog and event default scopes, add favicon

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,4 +5,6 @@ class Blog < ActiveRecord::Base
   validates :user_id, presence: true
 
   belongs_to :user
+
+  default_scope order("created_at DESC")
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,6 @@ class Event < ActiveRecord::Base
   validates :user_id, presence: true
 
   belongs_to :user
+
+  default_scope order("created_at ASC")
 end
