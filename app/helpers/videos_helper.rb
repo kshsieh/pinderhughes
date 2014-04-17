2e385df509d35e2cb167e07cf78243d60962756a
thwang <thetimothy@gmail.com>
2014-04-16 21:51:27 -0400

    allow flexibility with youtube link submission -embed code wont work if prefixed with http

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,6 +1,7 @@
 module VideosHelper
   def youtube_embed(link)
-    link.insert(0,'//')
+    link.sub!(/\A(https*:\/\/)/, '')
+    link.prepend('//')
     link.sub(/(watch\?v=)/, 'embed/')
   end
 end
